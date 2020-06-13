fb67b53ff466de8bf013ad4beb5afcb2d77368d3
msksaul <64514098+msksaul@users.noreply.github.com>
2020-06-12 21:03:09 -0600

    feature/context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = (props) => {  
+  const [token, setToken] = useState('');
+
+  return (
+    <AuthContext.Provider value={{
+        token, setToken
+    }}>
+      { props.children }
+    </AuthContext.Provider>
+  )
+};
+
+export default AuthContextProvider;
+
+/*
+ <Proveedor>
+    <Navbar>
+    <Body>
+    <Footer>
+<Proveedor>
+*/
